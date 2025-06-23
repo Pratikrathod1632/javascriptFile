93dc2a957df007297667e1dd6dc8a8add88c7c4d
Pratikrathod1632 <95957541+Pratikrathod1632@users.noreply.github.com>
2025-06-23 15:19:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("DJ Alok")
+
+var a=prompt("Enter your number")
+console.log("Number is "+a)
+
+var isTrue=confirm("Do you want to save")
+
+if(isTrue){
+    console.log("Saved")
+}
+else{
+    console.log("Not saved")
+}
